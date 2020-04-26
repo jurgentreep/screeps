1f4e453af8d1338b005bf7c0c6d85941577277ed
Jurgen Treep <jurgen.treep@gmail.com>
2020-04-26 16:29:36 +0200

    don't automatically active safe mode

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ export const loop = ErrorMapper.wrapLoop(() => {
     // }
 
     if (room.controller) {
-      room.controller.activateSafeMode();
+      // room.controller.activateSafeMode();
 
       defend(room);
 
